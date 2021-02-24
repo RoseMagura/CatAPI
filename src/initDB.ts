624b319cf8f6d586073ecc36ce657f44ad4cc3e2
RoseMagura <magurarm@guilford.edu>
2021-02-24 13:43:00 -0500

    Feat: Add some configuration for elastic beanstalk

--- src/initDB.ts
+++ src/initDB.ts
@@ -23,7 +23,8 @@ export const setUp = async(): Promise<void> => {
         ['Smoothie', 'https://www.usmagazine.com/wp-content/uploads/2018/06/Smoothie-the-Cat-Instagram-zoom.jpg?quality=86&strip=all'],
         ['Together', 'https://i.pinimg.com/originals/7d/b7/e7/7db7e7414842d89ca7741009b10cc376.jpg'],
         ['Close Up', 'https://welovecatsandkittens.com/wp-content/uploads/2017/09/smoothie.jpg'],
-        ['Loaves', 'https://i.redd.it/z9hb84g2yxvz.jpg']
+        ['Loaves', 'https://i.redd.it/z9hb84g2yxvz.jpg'],
+        ['Bingus', 'https://pbs.twimg.com/media/EoXHx4cUwAAf8Wx.jpg']
     ];
 
     const currImages = await query('SELECT * FROM IMAGES');
